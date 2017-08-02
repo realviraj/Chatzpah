baa11a39899238d758b54327b3b2036edc82c2a0
Viraj <developer.viraj@gmail.com>
2017-08-02 17:26:12 -0400

    authentication services file added, constant created

--- chatzpah8/Constant.swift
+++ chatzpah8/Constant.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool) -> ()
+
+
+//URL
+let BASE_URL =  "https://chatzpah.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)accounts/resgister"
+
+
+//SEGUES
 let TO_LOGIN = "toLogin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+
+//USER DEFAULTS
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
